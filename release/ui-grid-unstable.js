23ee14136c0f2505fee36eba34302de3e6008b38
grud <nggridteam@gmail.com>
2014-02-06 23:45:32 +0000

    gh-pages v2.0.7-ca41ed8

--- release/ui-grid-unstable.js
+++ release/ui-grid-unstable.js
@@ -1,4 +1,4 @@
-/*! ui-grid - v2.0.7-4565b0b - 2014-02-04
+/*! ui-grid - v2.0.7-ca41ed8 - 2014-02-06
 * Copyright (c) 2014 ; Licensed MIT */
 (function () {
   'use strict';
@@ -792,6 +792,90 @@
         };
       }
     };
+  }]);
+
+})();
+(function(){
+  'use strict';
+
+  angular.module('ui.grid').directive('uiGridColumnResizer', ['$log', '$document', function ($log, $document) {
+    var resizeOverlay = angular.element('<div class="ui-grid-resize-overlay"></div>');
+
+    var resizer = {
+      priority: 0,
+      scope: {
+        col: '='
+      },
+      require: '?^uiGrid',
+      link: function ($scope, $elm, $attrs, uiGridCtrl) {
+        var startX = 0,
+            x = 0,
+            gridLeft = 0;
+
+        function mousemove(event, args) {
+          if (event.originalEvent) { event = event.originalEvent; }
+          event.preventDefault();
+
+          x = event.clientX - gridLeft;
+
+          if (x < 0) { x = 0; }
+          else if (x > uiGridCtrl.grid.gridWidth) { x = uiGridCtrl.grid.gridWidth; }
+
+          resizeOverlay.css({ left: x });
+        }
+
+        function mouseup(event, args) {
+          if (event.originalEvent) { event = event.originalEvent; }
+          event.preventDefault();
+
+          uiGridCtrl.grid.element.removeClass('column-resizing');
+
+          resizeOverlay.remove();
+
+          // Resize the column
+
+          $document.off('mouseup', mouseup);
+          $document.off('mousemove', mousemove);
+        }
+
+        $elm.on('mousedown', function(event, args) {
+          if (event.originalEvent) { event = event.originalEvent; }
+          event.preventDefault();
+
+          uiGridCtrl.grid.element.addClass('column-resizing');
+
+          // Get the left offset of the grid
+          gridLeft = uiGridCtrl.grid.element[0].offsetLeft;
+
+          // Get the starting X position, which is the X coordinate of the click minus the grid's offset
+          startX = event.clientX - gridLeft;
+
+          // Append the resizer overlay
+          uiGridCtrl.grid.element.append(resizeOverlay);
+
+          // Place the resizer overlay at the start position
+          resizeOverlay.css({ left: startX });
+
+          // Add handlers for mouse move and up events
+          $document.on('mouseup', mouseup);
+          $document.on('mousemove', mousemove);
+        });
+
+        // On doubleclick, resize to fit all rendered cells
+        $elm.on('dblclick', function() {
+
+        });
+
+        $elm.on('$destroy', function() {
+          $elm.off('mousedown');
+          $elm.off('dblclick');
+          $document.off('mousemove', mousemove);
+          $document.off('mouseup', mouseup);
+        });
+      }
+    };
+
+    return resizer;
   }]);
 
 })();
@@ -2899,53 +2983,6 @@ module.filter('px', function() {
     }
   };
 });
-
-})();
-(function(){
-  'use strict';
-
-  angular.module('ui.grid').directive('uiGridColumnResizer', ['$log', '$document', function ($log, $document) {
-    var resizer = {
-      priority: 0,
-      scope: {
-        col: '='
-      },
-      require: '?^uiGrid',
-      link: function ($scope, $elm, $attrs) {
-        function mousemove(event, args) {
-
-        }
-
-        function mouseup(event, args) {
-          // Resize the column
-
-          $document.off('mouseup', mouseup);
-          $document.off('mousemove', mousemove);
-        }
-
-        $elm.on('mousedown', function(event, args) {
-          event.preventDefault();
-
-          $document.on('mouseup', mouseup);
-          $document.on('mousemove', mousemove);
-        });
-
-        // On doubleclick, resize to fit all rendered cells
-        $elm.on('dblclick', function() {
-
-        });
-
-        $elm.on('$destroy', function() {
-          $elm.off('mousedown');
-          $elm.off('dblclick');
-          $document.off('mousemove', mousemove);
-          $document.off('mouseup', mouseup);
-        });
-      }
-    };
-
-    return resizer;
-  }]);
 
 })();
 (function () {
