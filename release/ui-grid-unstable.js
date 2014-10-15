7ed6b3fa88b284c09bc51a71edca0712de2c5dc6
grud <nggridteam@gmail.com>
2014-10-15 18:10:56 +0000

    gh-pages v3.0.0-rc.12-a138105

--- release/ui-grid-unstable.js
+++ release/ui-grid-unstable.js
@@ -1,4 +1,4 @@
-/*! ui-grid - v3.0.0-rc.12-91de254 - 2014-10-15
+/*! ui-grid - v3.0.0-rc.12-a138105 - 2014-10-15
 * Copyright (c) 2014 ; License: MIT */
 (function () {
   'use strict';
@@ -9296,6 +9296,73 @@ module.filter('px', function() {
     }]);
 }]);
 })();
+(function () {
+  angular.module('ui.grid').config(['$provide', function($provide) {
+    $provide.decorator('i18nService', ['$delegate', function($delegate) {
+      $delegate.add('fi', {
+        aggregate: {
+          label: 'rivit'
+        },
+        groupPanel: {
+          description: 'Raahaa ja pudota otsikko tähän ryhmittääksesi sarakkeen mukaan.'
+        },
+        search: {
+          placeholder: 'Hae...',
+          showingItems: 'Näytetään rivejä:',
+          selectedItems: 'Valitut rivit:',
+          totalItems: 'Rivejä yht.:',
+          size: 'Näytä:',
+          first: 'Ensimmäinen sivu',
+          next: 'Seuraava sivu',
+          previous: 'Edellinen sivu',
+          last: 'Viimeinen sivu'
+        },
+        menu: {
+          text: 'Valitse sarakkeet:'
+        },
+        sort: {
+          ascending: 'Järjestä nouseva',
+          descending: 'Järjestä laskeva',
+          remove: 'Poista järjestys'
+        },
+        column: {
+          hide: 'Piilota sarake'
+        },
+        aggregation: {
+          count: 'Rivejä yht.: ',
+          sum: 'Summa: ',
+          avg: 'K.a.: ',
+          min: 'Min: ',
+          max: 'Max: '
+        },
+        pinning: {
+         pinLeft: 'Lukitse vasemmalle',
+          pinRight: 'Lukitse oikealle',
+          unpin: 'Poista lukitus'
+        },
+        gridMenu: {
+          columns: 'Sarakkeet:',
+          importerTitle: 'Tuo tiedosto',
+          exporterAllAsCsv: 'Vie tiedot csv-muodossa',
+          exporterVisibleAsCsv: 'Vie näkyvä tieto csv-muodossa',
+          exporterSelectedAsCsv: 'Vie valittu tieto csv-muodossa',
+          exporterAllAsPdf: 'Vie tiedot pdf-muodossa',
+          exporterVisibleAsPdf: 'Vie näkyvä tieto pdf-muodossa',
+          exporterSelectedAsPdf: 'Vie valittu tieto pdf-muodossa'
+        },
+        importer: {
+          noHeaders: 'Sarakkeen nimiä ei voitu päätellä, onko tiedostossa otsikkoriviä?',
+          noObjects: 'Tietoja ei voitu lukea, onko tiedostossa muuta kuin otsikkot?',
+          invalidCsv: 'Tiedostoa ei voitu käsitellä, oliko se CSV-muodossa?',
+          invalidJson: 'Tiedostoa ei voitu käsitellä, oliko se JSON-muodossa?',
+          jsonNotArray: 'Tiedosto ei sisältänyt taulukkoa, lopetetaan.'
+        }
+      });
+      return $delegate;
+    }]);
+  }]);
+})();
+
 (function () {
   angular.module('ui.grid').config(['$provide', function($provide) {
     $provide.decorator('i18nService', ['$delegate', function($delegate) {
