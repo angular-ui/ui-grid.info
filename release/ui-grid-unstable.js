3a80fe7bd218331153aaadc45ae66ac886d54d5b
grud <nggridteam@gmail.com>
2014-11-03 03:31:08 +0000

    gh-pages v3.0.0-rc.12-0845f09

--- release/ui-grid-unstable.js
+++ release/ui-grid-unstable.js
@@ -1,4 +1,4 @@
-/*! ui-grid - v3.0.0-rc.12-c280b35 - 2014-11-02
+/*! ui-grid - v3.0.0-rc.12-0845f09 - 2014-11-03
 * Copyright (c) 2014 ; License: MIT */
 (function () {
   'use strict';
@@ -9386,13 +9386,13 @@ module.filter('px', function() {
           label: 'Artículos'
         },
         groupPanel: {
-          description: 'Arrastre un encabezado de columna aquí y soltarlo para agrupar por esa columna.'
+          description: 'Arrastre un encabezado de columna aquí y suéltelo para agrupar por esa columna.'
         },
         search: {
           placeholder: 'Buscar...',
-          showingItems: 'Artículos Mostrando:',
+          showingItems: 'Artículos Mostrados:',
           selectedItems: 'Artículos Seleccionados:',
-          totalItems: 'Artículos Totales:',
+          totalItems: 'Total Artículos:',
           size: 'Tamaño de Página:',
           first: 'Primera Página',
           next: 'Página Siguiente',
@@ -9406,34 +9406,35 @@ module.filter('px', function() {
           hide: 'Ocultar la columna'
         },
         aggregation: {
-          count: 'total rows: ',
+          count: 'total filas: ',
           sum: 'total: ',
-          avg: 'avg: ',
+          avg: 'media: ',
           min: 'min: ',
           max: 'max: '
         },
         gridMenu: {
-          columns: 'Columns:',
-          importerTitle: 'Import file',
-          exporterAllAsCsv: 'Export all data as csv',
-          exporterVisibleAsCsv: 'Export visible data as csv',
-          exporterSelectedAsCsv: 'Export selected data as csv',
-          exporterAllAsPdf: 'Export all data as pdf',
-          exporterVisibleAsPdf: 'Export visible data as pdf',
-          exporterSelectedAsPdf: 'Export selected data as pdf'
+          columns: 'Columnas:',
+          importerTitle: 'Importar archivo',
+          exporterAllAsCsv: 'Exportar todo como csv',
+          exporterVisibleAsCsv: 'Exportar vista como csv',
+          exporterSelectedAsCsv: 'Exportar selección como csv',
+          exporterAllAsPdf: 'Exportar todo como pdf',
+          exporterVisibleAsPdf: 'Exportar vista como pdf',
+          exporterSelectedAsPdf: 'Exportar selección como pdf'
         },
         importer: {
-          noHeaders: 'Column names were unable to be derived, does the file have a header?',
-          noObjects: 'Objects were not able to be derived, was there data in the file other than headers?',
-          invalidCsv: 'File was unable to be processed, is it valid CSV?',
-          invalidJson: 'File was unable to be processed, is it valid Json?',
-          jsonNotArray: 'Imported json file must contain an array, aborting.'
+          noHeaders: 'No fue posible derivar nombres de columnas, ¿tiene encabezados el archivo?',
+          noObjects: 'No fue posible obtener registros, ¿existe información aparte de los encabezados?',
+          invalidCsv: 'No fue posible procesar el archivo, ¿es un CSV válido?',
+          invalidJson: 'No fue posible procesar el archivo, ¿es un Json válido?',
+          jsonNotArray: 'Archivo json importado debe contener un arreglo, abortando.'
         }
       });
       return $delegate;
     }]);
 }]);
 })();
+
 (function () {
   angular.module('ui.grid').config(['$provide', function($provide) {
     $provide.decorator('i18nService', ['$delegate', function($delegate) {
