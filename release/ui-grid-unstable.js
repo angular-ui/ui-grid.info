622b041ed7354f9b5588807f9454567d913666b2
grud <nggridteam@gmail.com>
2015-02-26 21:18:10 +0000

    gh-pages v3.0.0-rc.20-abe55b9

--- release/ui-grid-unstable.js
+++ release/ui-grid-unstable.js
@@ -1,5 +1,5 @@
 /*!
- * ui-grid - v3.0.0-rc.20 - 2015-02-24
+ * ui-grid - v3.0.0-rc.20-abe55b9 - 2015-02-26
  * Copyright (c) 2015 ; License: MIT 
  */
 
@@ -10478,6 +10478,73 @@ module.filter('px', function() {
     }]);
   }]);
 })();
+(function () {
+  angular.module('ui.grid').config(['$provide', function($provide) {
+    $provide.decorator('i18nService', ['$delegate', function($delegate) {
+      $delegate.add('hy', {
+        aggregate: {
+          label: 'տվյալներ'
+        },
+        groupPanel: {
+          description: 'Ըստ սյան խմբավորելու համար քաշեք և գցեք վերնագիրն այստեղ։'
+        },
+        search: {
+          placeholder: 'Փնտրում...',
+          showingItems: 'Ցուցադրված տվյալներ՝',
+          selectedItems: 'Ընտրված:',
+          totalItems: 'Ընդամենը՝',
+          size: 'Տողերի քանակը էջում՝',
+          first: 'Առաջին էջ',
+          next: 'Հաջորդ էջ',
+          previous: 'Նախորդ էջ',
+          last: 'Վերջին էջ'
+        },
+        menu: {
+          text: 'Ընտրել սյուները:'
+        },
+        sort: {
+          ascending: 'Աճման կարգով',
+          descending: 'Նվազման կարգով',
+          remove: 'Հանել '
+        },
+        column: {
+          hide: 'Թաքցնել սյունը'
+        },
+        aggregation: {
+          count: 'ընդամենը տող՝ ',
+          sum: 'ընդամենը՝ ',
+          avg: 'միջին՝ ',
+          min: 'մին՝ ',
+          max: 'մաքս՝ '
+        },
+        pinning: {
+          pinLeft: 'Կպցնել ձախ կողմում',
+          pinRight: 'Կպցնել աջ կողմում',
+          unpin: 'Արձակել'
+        },
+        gridMenu: {
+          columns: 'Սյուներ:',
+          importerTitle: 'Ներմուծել ֆայլ',
+          exporterAllAsCsv: 'Արտահանել ամբողջը CSV',
+          exporterVisibleAsCsv: 'Արտահանել երևացող տվյալները CSV',
+          exporterSelectedAsCsv: 'Արտահանել ընտրված տվյալները CSV',
+          exporterAllAsPdf: 'Արտահանել PDF',
+          exporterVisibleAsPdf: 'Արտահանել երևացող տվյալները PDF',
+          exporterSelectedAsPdf: 'Արտահանել ընտրված տվյալները PDF'
+        },
+        importer: {
+          noHeaders: 'Հնարավոր չեղավ որոշել սյան վերնագրերը։ Արդյո՞ք ֆայլը ունի վերնագրեր։',
+          noObjects: 'Հնարավոր չեղավ կարդալ տվյալները։ Արդյո՞ք ֆայլում կան տվյալներ։',
+          invalidCsv: 'Հնարավոր չեղավ մշակել ֆայլը։ Արդյո՞ք այն վավեր CSV է։',
+          invalidJson: 'Հնարավոր չեղավ մշակել ֆայլը։ Արդյո՞ք այն վավեր Json է։',
+          jsonNotArray: 'Ներմուծված json ֆայլը պետք է պարունակի զանգված, կասեցվում է։'
+        }
+      });
+      return $delegate;
+    }]);
+  }]);
+})();
+
 (function () {
   angular.module('ui.grid').config(['$provide', function($provide) {
     $provide.decorator('i18nService', ['$delegate', function($delegate) {
