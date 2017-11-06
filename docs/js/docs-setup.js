7f474ce55387e912a7b8f868ee8c67ed67e85b61
Portugal, Marcelo <Marcelo.Portugal@Kronos.com>
2017-11-06 11:08:27 -0500

    gh-pages v4.0.8

--- docs/js/docs-setup.js
+++ docs/js/docs-setup.js
@@ -911,7 +911,7 @@ NG_DOCS={
       "type": "service",
       "moduleName": "ui.grid.i18n",
       "shortDescription": "Services for i18n",
-      "keywords": "$broadcasts $rootscope add adds aggregate api application ascending cache column current currently decorate description drag drop en fr getalllangs getcurrentlang getsafetext grid group grouped grouppanel header i18n i18nservice items label1 label2 lang language languages loaded map maps missing names path property retrieving return returns service services set setcurrentlang sets sort string stringmaps strings text translation ui update_event"
+      "keywords": "$broadcasts $rootscope add adds aggregate api application ascending cache column current currently decorate description drag drop en fr getalllangs getcurrentlang getsafetext grid group grouped grouppanel header i18n i18nconstants i18nservice items label1 label2 lang language languages loaded map maps missing names path property retrieving return returns service services set setcurrentlang sets sort string stringmaps strings text translation ui update_event"
     },
     {
       "section": "api",
